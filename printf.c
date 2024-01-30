e4dad0fadd9ee653e24d413dff8a516f0d248828
Kalkidan-Amare <kalkidanamare11a@gmail.com>
2024-01-30 11:41:16 -0800

    task 1

--- printf.c
+++ printf.c
@@ -41,6 +41,12 @@ int _printf(const char *format, ...)
 				i++;
 				length += count - 2;
 			}
+			else if (format[i + 1] == 'd' || format[i + 1] == 'i')
+            		{
+                		temp = va_arg(args, int);
+                		length += print_number(temp);
+                		i++;
+            		}
 			else
 				write(1, (format + i), 1);
 		else
@@ -49,4 +55,37 @@ int _printf(const char *format, ...)
 	}
 	va_end(args);
 	return (length);
+}
+
+/**
+ * print_number - prints an integer and returns its length
+ * @n: the integer to be printed
+ *
+ * Return: the number of digits in the integer
+ */
+
+int print_number(int n)
+{
+    int length = 0;
+
+    if (n < 0)
+    {
+        write(1, "-", 1);
+        n = -n;
+        length++;
+    }
+
+    if (n == 0)
+    {
+        write(1, "0", 1);
+        return 1;
+    }
+
+    if (n / 10)
+        length += print_number(n / 10);
+
+    char digit = (char)(n % 10 + '0');
+    write(1, &digit, 1);
+
+    return length + 1;
 }
